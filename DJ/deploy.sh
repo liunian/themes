8b0c157b18d006fc0c0ab777526990c7a0eba12d
liunian <dengjij@gmail.com>
2012-05-01 19:36:15 +0800

    remove temp js files

--- DJ/deploy.sh
+++ DJ/deploy.sh
@@ -9,5 +9,8 @@ echo "min user.js to user.min.js"
 echo "min comments-ajax.js to comments-ajax.min.js"
 `java -jar ../yuicompressor-2.4.7.jar comments-ajax.js -o comments-ajax.min.js`
 
-echo "concat jquery-1.5.min.js, user.js, comments-ajax.min.js to dj.js"
+echo "concat jquery-1.5.min.js, user.js, comments-ajax.min.js to dj.min.js"
 `cat jquery-1.5.min.js user.min.js comments-ajax.min.js > dj.min.js`
+
+echo "remove user.min.js, comments-ajax.min.js"
+`rm -f user.min.js comments-ajax.min.js`
