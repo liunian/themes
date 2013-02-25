89b7c482a3f261faca029473928e291621919994
liunian <dengjij@gmail.com>
2013-02-25 23:58:26 +0800

    modify command

--- DJ/deploy.sh
+++ DJ/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "min style.css to style.min.css"
-`java -jar ../yuicompressor-2.4.7.jar --type css style.css -o style.min.css`
+`yuicompressor  --type css style.css -o style.min.css`
 
 echo "min user.js to user.min.js"
-`java -jar ../yuicompressor-2.4.7.jar user.js -o user.min.js`
+`yuicompressor user.js -o user.min.js`
 
 echo "min comments-ajax.js to comments-ajax.min.js"
-`java -jar ../yuicompressor-2.4.7.jar comments-ajax.js -o comments-ajax.min.js`
+`yuicompressor comments-ajax.js -o comments-ajax.min.js`
 
 echo "concat jquery-1.5.min.js, user.js, comments-ajax.min.js to dj.min.js"
 `cat jquery-1.5.min.js user.min.js comments-ajax.min.js > dj.min.js`
